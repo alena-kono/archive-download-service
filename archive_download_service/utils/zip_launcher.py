37409e05d683cc1274f720702e72c5524dc52d43
Alena Konovalova <konovalova.alena@gmail.com>
2022-04-11 00:18:42 +0300

    Lauch zip util quietly

--- archive_download_service/utils/zip_launcher.py
+++ archive_download_service/utils/zip_launcher.py
@@ -3,7 +3,7 @@ from asyncio.subprocess import Process
 
 
 async def create_zip_util_process(dir_to_be_zipped: str) -> Process:
-    cmd = "zip -rj - {dir}".format(dir=dir_to_be_zipped)
+    cmd = "zip -rjq - {dir}".format(dir=dir_to_be_zipped)
     return await asyncio.create_subprocess_shell(
             cmd,
             stdout=asyncio.subprocess.PIPE,
